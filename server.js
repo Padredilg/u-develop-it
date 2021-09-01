eb78319cc9ca67a0d6d1322f35c868733fb1459f
Padredilg <padredilg@knights.ucf.edu>
2021-09-01 15:58:03 -0400

    created variables to call the database, installed .env, and wrote a query call

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
 
 //add port designation and app expression
 const PORT = process.env.PORT || 3001;
@@ -8,12 +10,28 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//this gets info to show on port
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    //a query is a request to the database
+    //rows gets the value returned from the request written on the left
+    //if a table is returned it becomes an object
+    console.log(rows);
+})
 
 
 //Default response for any other request (Not Found)
 //goes through if /extension is not defined. keep it last!
-app.use((req, res) => {
+app.use((req, res) => {//also called catchall route
     res.status(404).end();
   });
 
